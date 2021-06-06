923c74f8fc24edb7a4db93334afd8fd24c372435
Dylan Gatlin <dgatlin@apo.nmsu.edu>
2021-06-05 19:28:26 -0600

    Improvements based on use

--- rsync_music.py
+++ rsync_music.py
@@ -1,25 +1,34 @@
 import json
 import argparse
+import copy
 import subprocess as sub
 from pathlib import Path
+from astropy.time import Time
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--dry-run", action="store_true",
                         help="Passed on to rsync for testing")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                        help="Verbose printing, also passed to rsync")
     return parser.parse_args()
 
 
 def main(args=parse_args()):
+    t_start = Time.now()
     here = Path(__file__).absolute().parent
     config_path = here / "to_sync.json"
 
     config = json.load(config_path.open('r'))
+    outfig = copy.deepcopy(config)
 
     source_flac_path = config["source_flac_path"]
     destination_flac_path = config["destination_flac_path"]
     dest_machine = config["destination_machine"]
+    if config["last_sync"] == t_start.isot[:10]:
+        raise ValueError("Last sync was today, not syncing")
+
     x = sub.check_output(f'ping -c 1 "{dest_machine.strip("pi@")}"', shell=True
                          ).decode("utf-8")
     # print(x)
@@ -27,35 +36,61 @@ def main(args=parse_args()):
         raise Exception(f"Remote host {dest_machine} not found")
 
     for src in config["unsynced_paths"]:
+        if ((Time.now() - t_start) * 24).value > 5:
+            print("Ran out of time")
+            exit()
         source = Path(src)
+        suffix_path = source.relative_to(source_flac_path)
+        print(f"Starting {suffix_path}")
+
         if not source.is_dir():
             print(f"    Removing {source}, not a directory")
-            config["unsynced_paths"].remove(src)
+            outfig["unsynced_paths"].remove(src)
             if not args.dry_run:
-                json.dump(config, config_path.open('w'))
+                with config_path.open('w') as fil:
+                    json.dump(outfig, fil)
             continue
 
-        suffix_path = source.relative_to(source_flac_path)
-        kwargs = " --dry-run" if args.dry_run else ""
-        cmd = (f'rsync -Pavzh --ignore-existing{kwargs} "{source.as_posix()}/"'
+        dry_run = " --dry-run" if args.dry_run else ""
+
+        v = "v" if args.verbose else  ""
+        dest = (destination_flac_path / suffix_path).as_posix().replace(
+                " ", "\\ ")
+        cmd = (f'rsync -Pa{v}zh --ignore-existing{dry_run} "{source.as_posix()}/"'
                f' "{dest_machine}'
-               f':{destination_flac_path / suffix_path}/"')
+               f':{dest}"')
         try:
-            print(cmd)
+            if args.verbose:
+                print(cmd)
             cmd_output = sub.run(cmd, shell=True, check=False, capture_output=True)
-            # print(cmd_output.stdout)
+            if args.verbose:
+                print("stdout: ", cmd_output.stdout.decode("utf-8"))
+                print("stderr: ", cmd_output.stderr.decode("utf-8"))
+            
             if cmd_output.returncode == 0:
                 print(f"    Completed {suffix_path}")
-                config["unsynced_paths"].remove(src)
+                outfig["unsynced_paths"].remove(src)
                 if not args.dry_run:
-                    json.dump(config, config_path.open('w'))
-
-        except FileNotFoundError:
-            print("    Removing {source}, path not found")
-            config["unsynced_paths"].remove(src)
-            if not args.dry_run:
-                json.dump(config, config_path.open('w'))
+                    with config_path.open('w') as fil:
+                        json.dump(outfig, fil)
+        # Originally I thought this could be used if the path
+        # isn't found, but this only came up when shell=True
+        # wasn't used in sub.run (ie. rsync is not found as a
+        # file
+        # except FileNotFoundError as e:
+        #     print("    FileNotFoundError {e}")
+        #     outfig["unsynced_paths"].remove(src)
+        #     if not args.dry_run:
+        #         with config_path.open('w') as fil:
+        #             json.dump(outfig, fil)
+        #     continue
+        except Exception as e:
+            print(f"    Failed: {e}")
             continue
+
+    outfig["last_sync"] = Time.now().isot[:10]
+    if not args.dry_run:
+        json.dump(outfig, config_path.open('w'))
 
 
 if __name__ == "__main__":
